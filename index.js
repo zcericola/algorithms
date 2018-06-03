2fc735d51cb922591764f46bd276acec2951392e
Zac Cericola <zcericola@gmail.com>
2018-06-03 09:51:07 -0500

    solved permutation problem

--- index.js
+++ index.js
@@ -15,16 +15,31 @@ return true;
 //isUnique('abcdefg') returns true
 //isUnique('abccdefg') returns false
 
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
 //Problem 2 
 //Given two strings, write a method to decide if one is a permutation of the other.
 
+//sorts the strings alphabetically
+const sortByLetter = (str) => {
+    return str.split('').sort().join('');
+}
+
 const isPerm = (s1, s2) => {
+    sortS1 = sortByLetter(s1);
+    sortS2 = sortByLetter(s2);
+//return false if they are varying lengths
     if(s1.length !== s2.length){
         return false;
     }
-
-    
-
-
-
+    //if sortS1 and sortS2 are triple equals, true will be returned, else we will get false
+    return sortS1 === sortS2;
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+//Problem 3
+
+
